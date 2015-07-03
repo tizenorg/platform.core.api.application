eadc16839358e524a5e74fdab65bbe5cc2de61ae
Jiwoong Im <jiwoong.im@samsung.com>
2015-07-03 16:20:29 +0900

    remove ail dependency
    - replace ail api to pkgmgr-info api.
    
    Change-Id: I014aa13dcc3f962838f68a4396663ea5a2ef80f8
    Signed-off-by: Jiwoong Im <jiwoong.im@samsung.com>

--- app_common/app_package.c
+++ app_common/app_package.c
@@ -11,22 +11,16 @@
  * distributed under the License is distributed on an AS IS BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-#include <bundle.h>
-#include <appcore-common.h>
 #include <aul.h>
-#include <ail.h>
+#include <pkgmgr-info.h>
 #include <dlog.h>
 
 #include <app_private.h>
@@ -43,26 +37,20 @@ int app_get_package_app_name(const char *appid, char **name)
 	char *name_token = NULL;
 
 	if (appid == NULL)
-	{
 		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
-	}
 
 	// com.vendor.name -> name
 	name_token = strrchr(appid, '.');
 
 	if (name_token == NULL)
-	{
 		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, NULL);
-	}
 
 	name_token++;
 
 	*name = strdup(name_token);
 
 	if (*name == NULL)
-	{
 		return app_error(APP_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
-	}
 
 	return APP_ERROR_NONE;
 }
@@ -78,111 +66,113 @@ int app_get_id(char **id)
 	int ret = -1;
 
 	if (id == NULL)
-	{
 		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
-	}
 
-	if (id_buf[0] == '\0')
-	{
+	if (id_buf[0] == '\0') {
 		ret = aul_app_get_appid_bypid(getpid(), id_buf, sizeof(id_buf));
-		if (ret < 0) {
+		if (ret < 0)
 			return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the application ID");
-		}
 	}
 
 	if (id_buf[0] == '\0')
-	{
 		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the application ID");
-	}
 
 	*id = strdup(id_buf);
 
 	if (*id == NULL)
-	{
 		return app_error(APP_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
-	}
 
-	return APP_ERROR_NONE;
-}
-
-static int app_get_appinfo(const char *package, const char *property, char **value)
-{
-	ail_appinfo_h appinfo;
-	char *appinfo_value;
-	char *appinfo_value_dup;
-
-	if (ail_get_appinfo(package, &appinfo) != 0)
-	{
-		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get app-info");
-	}
-	
-	if (ail_appinfo_get_str(appinfo, property, &appinfo_value) != 0)
-	{
-		ail_destroy_appinfo(appinfo);
-		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get app-property");
-	}
-
-	appinfo_value_dup = strdup(appinfo_value);
-
-	ail_destroy_appinfo(appinfo);
-
-	if (appinfo_value_dup == NULL)
-	{
-		return app_error(APP_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
-	}
-
-	*value = appinfo_value_dup;
-	
 	return APP_ERROR_NONE;
 }
 
 int app_get_name(char **name)
 {
-	char *package = NULL;
-	int retval;
+	int retval = 0;
+	char *appid = NULL;
+	char *label = NULL;
+	pkgmgrinfo_appinfo_h appinfo = NULL;
 
 	if(name == NULL)
-	{
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
+
+	if (app_get_id(&appid) != 0)
+		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the package");
+
+	retval = pkgmgrinfo_appinfo_get_usr_appinfo(appid, getuid(), &appinfo);
+	if (retval != 0) {
+		free(appid);
 		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
 	}
 
-	if (app_get_id(&package) != 0)
-	{
-		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the package");
+	retval = pkgmgrinfo_appinfo_get_label(appinfo, &label);
+	if (retval != 0) {
+		free(appid);
+		pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
 	}
 
-	retval = app_get_appinfo(package, AIL_PROP_NAME_STR, name);
+	*name = strdup(label);
+	pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+	free(appid);
 
-	if (package != NULL)
-	{
-		free(package);
-	}
+	if (*name == NULL)
+		return app_error(APP_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
 
-	return retval;
+	return APP_ERROR_NONE;
 }
 
 int app_get_version(char **version)
 {
-	char *package;
-	int retval;
+	int retval = 0;
+	char *appid = NULL;
+	char *pkgid = NULL;
+	char *pkg_version = NULL;
+	pkgmgrinfo_pkginfo_h pkginfo = NULL;
+	pkgmgrinfo_appinfo_h appinfo = NULL;
 
 	if(version == NULL)
-	{
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
+
+	if (app_get_id(&appid) != 0)
+		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the package");
+
+	retval = pkgmgrinfo_appinfo_get_usr_appinfo(appid, getuid(), &appinfo);
+	if (retval != 0) {
+		free(appid);
 		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
 	}
 
-	if (app_get_id(&package) != 0)
-	{
-		return app_error(APP_ERROR_INVALID_CONTEXT, __FUNCTION__, "failed to get the package");
+	retval = pkgmgrinfo_appinfo_get_pkgid(appinfo, &pkgid);
+	if (retval != 0) {
+		free(appid);
+		pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
 	}
 
-	retval = app_get_appinfo(package, AIL_PROP_VERSION_STR, version);
-
-	if (package != NULL)
-	{
-		free(package);
+	retval = pkgmgrinfo_pkginfo_get_pkginfo(pkgid, &pkginfo);
+	if (retval != 0) {
+		free(appid);
+		pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
 	}
 
-	return retval;	
+	retval = pkgmgrinfo_pkginfo_get_version(pkginfo, &pkg_version);
+	if (retval != 0) {
+		free(appid);
+		pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+		pkgmgrinfo_pkginfo_destroy_pkginfo(pkginfo);
+		return app_error(APP_ERROR_INVALID_PARAMETER, __FUNCTION__, NULL);
+	}
+
+	*version = strdup(pkg_version);
+	pkgmgrinfo_pkginfo_destroy_pkginfo(pkginfo);
+	pkgmgrinfo_appinfo_destroy_appinfo(appinfo);
+	free(appid);
+
+	if (*version == NULL)
+		return app_error(APP_ERROR_OUT_OF_MEMORY, __FUNCTION__, NULL);
+
+	return APP_ERROR_NONE;
 }
+
 
