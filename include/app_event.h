cb13ce50b9af1cdddabfab7887b7b0413bac6ded
Jiwoong Im <jiwoong.im@samsung.com>
2016-07-01 15:38:18 +0900

    add event_keep_last_event_data api
    - keep last User-Event data for receiver applications
    
    Change-Id: If3e00902a943cc2777c34dc38cf2f73c9db43bc2
    Signed-off-by: Jiwoong Im <jiwoong.im@samsung.com>

--- include/app_event.h
+++ include/app_event.h
@@ -933,6 +933,21 @@ int event_publish_app_event(const char *event_name, bundle *event_data);
 int event_publish_trusted_app_event(const char *event_name, bundle *event_data);
 
 /**
+ * @brief Keeps last User-Event data for receiver applications.
+ *
+ * @since_tizen 3.0
+ * @remarks The receiver applications will receive this last event data after adding their new handlers via event_add_event_handler() API since the sender application called this API.
+ * @remarks If a sender application sends same event via trusted API and non-trusted API, then a trusted receiver will get latest data regardless of trusted or non-trusted, but non-trusted receiver will get the last data only from non-trusted API.
+ * @param[in] event_name The event's name to keep last event data
+ * @return 0 on success, otherwise a negative error value
+ * @retval #EVENT_ERROR_NONE Successful
+ * @retval #EVENT_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #EVENT_ERROR_IO_ERROR Sending operation failed
+ * @retval #EVENT_ERROR_OUT_OF_MEMORY Out of memory
+ */
+int event_keep_last_event_data(const char *event_name);
+
+/**
  * @}
  */
 
